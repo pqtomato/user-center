cb5be0cac1b54daba0df71aef9b61cf2e4c6f177
pqtomato <897101935@qq.com>
2021-02-22 18:22:52 +0800

    项目搭建

--- src/main/java/com/itmuch/usercenter/UserCenterApplication.java
+++ src/main/java/com/itmuch/usercenter/UserCenterApplication.java
@@ -1,8 +1,11 @@
 package com.itmuch.usercenter;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//扫描mybatis哪些接口
+@MapperScan("com.itmuch")
 @SpringBootApplication
 public class UserCenterApplication {
 
